4d83ba3a494a711c052301f3a8ff6b084c00382b
michaelhartMU <74085767+michaelhartMU@users.noreply.github.com>
2020-11-12 12:00:13 +1100

    Add files via upload

--- checkItems.m
+++ checkItems.m
@@ -1,25 +1,29 @@
-function [item, expired, lowstock, outofstock] = checkItems(EPCASCCI)
+function [item, eDates, stockActual, stockFull, expired, lowstock, outofstock] = checkItems(EPCASCII)
 %test = {'item01201120200230', 'item02011120200030', 'item03201120212530', 'item03241020202830'};
 
 %pre-allocating
-expired = zeros(1,size(EPCASCCI,2));
-lowstock = zeros(1,size(EPCASCCI,2));
-outofstock = zeros(1,size(EPCASCCI,2));
+expired = zeros(1,size(EPCASCII,2));
+lowstock = zeros(1,size(EPCASCII,2));
+outofstock = zeros(1,size(EPCASCII,2));
 
-    for i=1:size(EPCASCCI,2)
-        itemString = char(EPCASCCI(i));
+    for i=1:size(EPCASCII,2)
+        itemString = char(EPCASCII(i));
+        itemString = itemString(~isspace(itemString)); %get rid of spaces
         
         % Item name
         item{i} = cellstr(itemString(1:6));
         
         % Checking the expiry date
         currentDate = today('datetime');
-        expiryDate = datetime(itemString(7:14),'InputFormat','ddMMyyyy');    
+        expiryDate = datetime(itemString(7:14),'InputFormat','ddMMyyyy');
+        eDates{i} = datestr(expiryDate);
         expired(i) = expiryDate < currentDate;
         
         % Checking stock levels
         stockLevel_Actual = str2double(itemString(15:16));
+        stockActual{i} = num2str(stockLevel_Actual);
         stockLevel_Full = str2double(itemString(17:18)); 
+        stockFull{i} = num2str(stockLevel_Full);
         
         lowstock(i) = (stockLevel_Actual/stockLevel_Full) < 0.20 && (stockLevel_Actual/stockLevel_Full) > 0;
         outofstock(i) = (stockLevel_Actual/stockLevel_Full) == 0;
--- scanShelf.m
+++ scanShelf.m
@@ -1,7 +1,7 @@
 function [EPC, EPCASCII] = scanShelf()
     % Tells the arduino to scan the shelf
 
-    s = serialport('COM3',115200);      %opens serial port to arduino
+    s = serialport('COM6',115200);      %opens serial port to arduino
     data = readline(s);                 %read opening line
     pause(0.5);                         %just gives the N6E Nano a little time to prepare serial port
     writeline(s,"start");       %tells N6E Nano to start reading
@@ -17,12 +17,12 @@ function [EPC, EPCASCII] = scanShelf()
     dataCell = cellstr(dataSplit);      %turns the strings into cells for manipulation
 
     for i = 1:size(dataCell,1)
-        EPC_all{i} = strjoin(dataSplit(i,4:end),' ');
+        EPC_all{i} = strjoin(dataSplit(i,4:end),'');
         EPCASCII_all{i} = strjoin(cellstr(char(hex2dec(dataCell(i,4:end)))));  %convert to ASCII text
     end
 
     % Finding unique tags by removing duplicates
-    EPC = cellstr(unique(string(EPC_all)))
-    EPCASCII = cellstr(unique(string(EPCASCII_all)))
+    EPC = cellstr(unique(string(EPC_all)));
+    EPCASCII = cellstr(unique(string(EPCASCII_all)));
 	
 end
